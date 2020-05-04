c6dbe95a883a84bc96c1bd36a3b982e5eb35c9e8
rpura92509 <rpura951@gmail.com>
2020-05-03 19:28:30 -0700

    Added to check if user is an admin

--- registration.php
+++ registration.php
@@ -1,5 +1,14 @@
 <?php
-    include 'verification.php';
+    include 'validation_fns.php';
+    session_start();
+
+    if ($_SESSION['isAdmin'] == 0)
+    {
+        echo('<script language="javascript">');
+        echo('alert("admin");
+        window.location.href="http://localhost/hr_system/main_page.html"');
+        echo('</script>');
+    }
 
     if(isset($_POST['Create']))
     {
@@ -10,15 +19,18 @@
         if (!validate_pw($_POST['tempPwd']))
         {
             echo("Password does not meet proper requirements");
+            exit;
         }
         else
         {
+            echo("Password is valid");
             $pw = password_hash($_POST['tempPwd'], PASSWORD_DEFAULT);
         }
         
         if (!validate_ssn($_POST['ssn']))
         {
             echo("SSN not valid");
+            exit;
         }
         else
         {
@@ -28,6 +40,7 @@
         if (!validate_phone($_POST['phone']))
         {
             echo("Phone number not valid");
+            exit;
         }
         else
         {
@@ -50,7 +63,7 @@
         if(mysqli_num_rows($result) > 0)
         {
             echo("Username already found");
-            header("Location: http://localhost/hr_system/registration.html");
+            header("Location: http://localhost/hr_system/registration.php");
             exit();
         }
         else
@@ -70,13 +83,88 @@
             if(!mysqli_query($db, $add_query))
             {
                 echo("Username and password not created</br>");
+                echo($un."username </br>");
+                echo($pw."password </br>");
             }
             else
             {
                 echo("User added to emp_credentials table</br>");
-                header("Location: http://localhost/hr_system/main_page.html");
+                header("Location: http://localhost/hr_system/main_page.php");
                 exit();
             }
         }
     }
 ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>User Registration</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <link href="css/registration.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <style>
+            th, td
+            {
+                padding:5px
+            }
+        </style>
+    </head>
+    
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-3 col-md-2 sidebar">
+                    <ul class="nav nav-sidebar">
+                        <h3>Menu</h3>
+                        <li><a href="#">Clock In/Out</a></li>
+                        <li><a href="#">Request Vacation</a></li>
+                        <li><a href="registration_page.html">Regsiter New User</a></li>
+                        <li><a href="logout.php">Log Out</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+            <h1 class="page-header">Register New User</h1>
+            <form method="post" enctype="multipart.form-data">
+                <table style="border: 70px;">
+                    <tr>
+                        <td sytle="width100px; text-align:left;">First Name</td>
+                        <td><input type="text" name="fname" required size="20" maxlength="20" /></td>
+                    </tr>
+                    <tr>
+                        <td sytle="width100px; text-align:left;">Last Name</td>
+                        <td><input type="text" name="lname" required size="20" maxlength="20" /></td>
+                    </tr>
+                        <td sytle="width100px; text-align:left;">Address</td>
+                        <td><input type="text" name="address" required size="20" /></td>
+                    </tr>
+                        <td sytle="width100px; text-align:left;">Phone Number</td>
+                        <td><input type="text" name="phone" required size="20" maxlength="20" /></td>
+                    </tr>
+                        <td sytle="width100px; text-align:left;">Username</td>
+                        <td><input type="text" name="username" required size="20" maxlength="20" /></td>
+                    </tr>
+                    <tr>
+                        <td sytle="width100px; text-align:left;">Temporary Password</td>
+                        <td><input type="text" name="tempPwd" required size="20" maxlength="20" /></td>
+                    </tr>
+                    <tr>
+                        <td sytle="width100px; text-align:left;">SSN</td>
+                        <td><input type="text" name="ssn" required size="20" maxlength="9" /></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><button type="submit" formaction="registration.php" name="Create">Create User</button></td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+    </body>
+</html>
