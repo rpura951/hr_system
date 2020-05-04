bf67935a806eb758636318045641c40d5d9f4b7e
rpura92509 <rpura951@gmail.com>
2020-05-03 19:27:06 -0700

    Updated to add isAdmin to session

--- login.php
+++ login.php
@@ -1,3 +1,53 @@
+<?php
+session_start();
+
+$db = mysqli_connect("localhost", "root", "", "hr_system");
+
+
+if(isset($_POST['verify']))
+{
+    $username = $_POST['emp_id'];
+    $password = $_POST['pwd'];
+    $query = "SELECT username, password FROM emp_credentials WHERE username = '$username'";
+    $result = $db->query($query);
+
+    if ($result->num_rows > 0)
+    {
+        $result = $result->fetch_assoc();
+        if (password_verify($password, $result['password']))
+        {
+            $query = "SELECT * FROM emp_data WHERE username = '$username'";
+            $result = $db->query($query);
+            $result = $result->fetch_assoc();
+            $_SESSION['fname'] = $result['fname'];
+            $_SESSION['isAdmin'] = $result['isAdmin'];
+            $_SESSION['username'] = $result['username'];
+            if($_SESSION['isAdmin'] == 1)
+            {
+                header("Location: admin.php");
+            }
+            else
+            {
+                header("Location: http://localhost/hr_system/main_page.php");
+            }
+            //echo($_SESSION['fname']);
+            // echo('<script language="javascript">');
+            // echo('alert("Login Successful");
+            // window.location.href="http://localhost/hr_system/main_page.html"');
+            // echo('</script>');
+        }
+    }
+    else
+    {
+        echo('<script language="javascript">');
+        echo('alert("Login Failed. Please Try Again.");
+        window.location.href="http://localhost/hr_system/login.html"');
+        echo('</script>');
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
@@ -64,44 +114,3 @@
     </body>
 </html>
 
-<?php
-session_start();
-
-$db = mysqli_connect("localhost", "root", "", "hr_system");
-
-
-if(isset($_POST['verify']))
-{
-    $username = $_POST['emp_id'];
-    $password = $_POST['pwd'];
-    $query = "SELECT username, password FROM emp_credentials WHERE username = '$username'";
-    $result = $db->query($query);
-
-    if ($result->num_rows > 0)
-    {
-        $result = $result->fetch_assoc();
-        if (password_verify($password, $result['password']))
-        {
-            $query = "SELECT * FROM emp_data WHERE username = '$username'";
-            $result = $db->query($query);
-            $result = $result->fetch_assoc();
-            $_SESSION['fname'] = $result['fname'];
-            $_SESSION['isAdmin'] = $result['isAdmin'];
-            //echo($_SESSION['fname']);
-            header("Location: http://localhost/hr_system/main_page.php");
-            // echo('<script language="javascript">');
-            // echo('alert("Login Successful");
-            // window.location.href="http://localhost/hr_system/main_page.html"');
-            // echo('</script>');
-        }
-    }
-    else
-    {
-        echo('<script language="javascript">');
-        echo('alert("Login Failed. Please Try Again.");
-        window.location.href="http://localhost/hr_system/login.html"');
-        echo('</script>');
-    }
-}
-
-?>
