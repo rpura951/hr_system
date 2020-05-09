f04401bcdfa0941b3173e9be88bcebff584d2ad4
unknown <limichael1099419@gmail.com>
2020-05-09 15:09:11 -0700

    login

---
+++ login.js
@@ -0,0 +1,34 @@
+$('#verify').on('click', function(event) {
+    event.preventDefault();
+    let username = $('#emp_id').val();
+    let password = $('#pwd').val(); 
+    console.log(username, password);
+
+    $.post('login.php', {
+        emp_id: username,
+        pwd: password
+    }, function(result) {
+        if(!result.success) {
+            $('#emp_id').val('');
+            $('#pwd').val('');
+            alert(result.error);
+            return;
+        }
+        event.preventDefault();
+        sessionStorage.setItem('username', username);
+        sessionStorage.setItem('fname', result.fname);
+        sessionStorage.setItem('isAdmin', result.isAdmin);
+
+        let relocation = (result.isAdmin === '0') ? 'main_page.php' : 'admin.php';
+        window.location.replace(relocation);
+    });
+});
+
+$('document').ready(function() {
+    let username = sessionStorage.getItem('username');
+    let isAdmin = sessionStorage.getItem('isAdmin');
+    if(username !== null) {
+        let relocation = (isAdmin === '0') ? 'main_page.php' : 'admin.php';
+        window.location.replace(relocation);
+    }
+});
