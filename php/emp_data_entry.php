1cf4319fcb005880ff389ed7a9145f60d7f5a8a2
rpura951 <rpura951@gmail.com>
2020-05-10 00:02:03 -0700

    Added error handling

--- php/emp_data_entry.php
+++ php/emp_data_entry.php
@@ -2,35 +2,44 @@
 
 function data_entry($db, $un, $today, $option) {
     $time = date('H:i:s');
-    if($option == 'start')
-    {   
+    try{
+        if($option == 'start'){   
         //new data table entry 
         $new_entry = "INSERT INTO timesheet (date, start, username, end, lunch_in, lunch_out, total_worked)
-                      VALUES ('$today', '$time', '$un', '00:00:00', '00:00:00', '00:00:00', 0)";
-        $db->query($new_entry);
+                    VALUES ('$today', '$time', '$un', '00:00:00', '00:00:00', '00:00:00', 0)";
+        $result = $db->query($new_entry);
 
-    }
-    else {
-        // update time for a table variable
-        $update_entry = "UPDATE timesheet SET {$option} = '$time' WHERE username = '$un' AND date = '$today'";
-        $db->query($update_entry);
-
-        //enter in total hours worked 
-        if($option == 'end') 
+        if (!$result)
         {
-            $calcQuery = "Set @start = (SELECT start FROM timesheet WHERE username = '$un' AND date = '$today');";
-            $db->query($calcQuery);
-            $calcQuery = "SET @end = (SELECT end FROM timesheet WHERE username = '$un' AND date = '$today');";    
-            $db->query($calcQuery);
-            $calcQuery = "SET @lunchstart = (SELECT lunch_out FROM timesheet WHERE username = '$un' AND date = '$today');";    
-            $db->query($calcQuery);
-            $calcQuery = "SET @lunchend = (SELECT lunch_in FROM timesheet WHERE username = '$un' AND date = '$today');";    
-            $db->query($calcQuery);
-            $calcQuery = "SET @total = TIME_TO_SEC(TIMEDIFF(@end, @start))/3600 - TIME_TO_SEC(TIMEDIFF(@lunchend, @lunchstart))/3600;";
-            $db->query($calcQuery);
-            $calcQuery = "UPDATE timesheet SET total_worked = ROUND(@total,2) WHERE username = '$un' AND date = '$today';";
-            $db->query($calcQuery);
+            throw new Exception("User unable to clock in.");
         }
-    }
+
+        }
+        else {
+            // update time for a table variable
+            $update_entry = "UPDATE timesheet SET {$option} = '$time' WHERE username = '$un' AND date = '$today'";
+            $db->query($update_entry);
+
+            //enter in total hours worked 
+            if($option == 'end') 
+            {
+                $calcQuery = "Set @start = (SELECT start FROM timesheet WHERE username = '$un' AND date = '$today');";
+                $db->query($calcQuery);
+                $calcQuery = "SET @end = (SELECT end FROM timesheet WHERE username = '$un' AND date = '$today');";    
+                $db->query($calcQuery);
+                $calcQuery = "SET @lunchstart = (SELECT lunch_out FROM timesheet WHERE username = '$un' AND date = '$today');";    
+                $db->query($calcQuery);
+                $calcQuery = "SET @lunchend = (SELECT lunch_in FROM timesheet WHERE username = '$un' AND date = '$today');";    
+                $db->query($calcQuery);
+                $calcQuery = "SET @total = TIME_TO_SEC(TIMEDIFF(@end, @start))/3600 - TIME_TO_SEC(TIMEDIFF(@lunchend, @lunchstart))/3600;";
+                $db->query($calcQuery);
+                $calcQuery = "UPDATE timesheet SET total_worked = ROUND(@total,2) WHERE username = '$un' AND date = '$today';";
+                $db->query($calcQuery);
+            }
+        }
     return $time;
+    } catch Exception($e){
+        $e->getMessage();
+    }
+    
 }
